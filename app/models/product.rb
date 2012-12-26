6cdc901ca45320136ee490757821fcfae679d5b0
Vandan Patel <vandan.patel@tstmedia.com>
2012-12-26 10:20:56 -0600

    testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  
 
   validates :description, :price, :title,  :presence => true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
 end
